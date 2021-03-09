fdf517d56b0f8e1cca449b18b9d2793d5b7af649
Jinzhu <wosmvp@gmail.com>
2021-03-09 17:50:07 +0800

    Fix copy slice, close #75

--- copier_test.go
+++ copier_test.go
@@ -243,6 +243,29 @@ func TestCopyFromSliceToSlice2(t *testing.T) {
 	}
 }
 
+type CollectionAlias struct {
+	CollectionName string `json:"collection_name"`
+	Name           string `json:"name"`
+}
+
+func createNewCollectionAlias(collectionName string, name string) *CollectionAlias {
+	return &CollectionAlias{
+		CollectionName: collectionName,
+		Name:           name,
+	}
+}
+
+func TestCopyFromSliceToSlice3(t *testing.T) {
+	expectedResult := []*CollectionAlias{
+		createNewCollectionAlias("collection", "collection_alias1"),
+		createNewCollectionAlias("collection", "collection_alias2"),
+		createNewCollectionAlias("collection", "collection_alias3"),
+	}
+
+	mockedResult := []*CollectionAlias{}
+	copier.Copy(&mockedResult, &expectedResult)
+}
+
 func TestEmbeddedAndBase(t *testing.T) {
 	type Base struct {
 		BaseField1 int
